c80b4b3710c8f5b29ea6680305415d1cdae75909
CraicOverflow89 <craic.overflow@hotmail.com>
2020-05-08 17:32:56 +0100

    Able to send email composed on site.

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from winston.app import Winston
+import os, re
 
 app = Flask(__name__)
 
@@ -6,6 +8,19 @@ app = Flask(__name__)
 def root():
 	return render_template("main.html")
 
-@app.route("/compose")
+@app.route("/compose", methods = ["GET", "POST"])
 def compose():
-	return render_template("compose.html")
+	if request.method == "POST":
+
+		# TEST SEND
+		def account_data():
+			with open(os.path.join(os.path.dirname(os.path.realpath("__file__")), "account")) as fs:
+				result = fs.read()
+			return re.split("\\|", result)
+		account, password = account_data()
+		Winston("smtp.live.com", 587, account, password).send(request.form['recipient'], request.form['subject'], request.form['content'])
+
+		# TEMP CONTENT
+		return "Message Sent"
+	else:
+		return render_template("compose.html")
