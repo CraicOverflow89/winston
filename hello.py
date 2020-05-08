edd6d653516b9a22f947c243c6c23aca4a17d8ef
CraicOverflow89 <craic.overflow@hotmail.com>
2020-05-08 17:14:39 +0100

    Added email compose form and external stylesheet.

--- hello.py
+++ hello.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def root():
 	return render_template("main.html")
+
+@app.route("/compose")
+def compose():
+	return render_template("compose.html")
